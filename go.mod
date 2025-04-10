420dc73403c8f9efabc62b35cc8ec5ba8ca414bd
Azavech <azavech@gmail.com>
2025-04-10 00:11:23 -0400

    First commit

--- go.mod
+++ go.mod
@@ -1,3 +1,21 @@
-module github.com/yeencloud/lib-library
+module github.com/yeencloud/lib-user
 
 go 1.23
+
+require github.com/yeencloud/lib-shared v0.2.4
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.26.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+)
+
+replace github.com/yeencloud/lib-shared => ../../lib-technical/lib-shared
---
+++ validator.go
@@ -0,0 +1,52 @@
+package lib_user
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/yeencloud/lib-shared/validation"
+)
+
+func userNameValidator(ctx context.Context, fl validator.FieldLevel) error {
+	username := fl.Field().String()
+
+	if len(username) < 3 || len(username) > 20 {
+		return fmt.Errorf("username must be between 3 and 20 characters long")
+	}
+
+	pattern := "^[a-zA-Z0-9_.-]+$"
+	re := regexp.MustCompile(pattern) // TODO: MustCompile should be used only once for performance
+
+	if !re.MatchString(username) {
+		return errors.New("username can only contain letters, digits, underscores, hyphens, or dots")
+	}
+
+	return nil
+}
+
+func validationCodeValidator(ctx context.Context, fl validator.FieldLevel) error {
+	validationCode := fl.Field().String()
+
+	if len(validationCode) != 6 {
+		return fmt.Errorf("validation code must be 6 characters long")
+	}
+
+	pattern := "^[0-9]+$"
+	re := regexp.MustCompile(pattern) // TODO: MustCompile should be used only once for performance
+
+	if !re.MatchString(validationCode) {
+		return errors.New("validation code can only contain digits")
+	}
+
+	return nil
+}
+
+func Validations() map[string]validation.ValidationFunc {
+	return map[string]validation.ValidationFunc{
+		"username":        userNameValidator,
+		"validation_code": validationCodeValidator,
+	}
+}
